67cf6b9f60d43135717a4c2e94f23bcdf7c0f3e6
Binyam <binyam@pop-os.localdomain>
2021-10-14 09:39:08 +0300

    Add src/store.js and update index.js
    - create redux store in store.js
    - add Provider wrapper around App to pass store as props

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './styles/global.css';
+import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
