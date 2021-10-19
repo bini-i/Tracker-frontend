3da3bd5899185b242126cbd82b853f683ffa7200
Binyam <binyam@pop-os.localdomain>
2021-10-19 09:39:24 +0300

    Update App and CreateBtn components
    - add createBtn component to App

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
 import {
   Redirect, Route, Switch,
+  useLocation,
 } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './styles/App.css';
 import NewSession from './components/NewSession';
 import NewAccount from './components/NewAccount';
 import MenuBar from './components/MenuBar';
+import CreateBtn from './components/CreateBtn';
 import Tasks from './components/Tasks';
 import { mapDispatchToProps, mapStateToProps } from './reducers';
+import NewTaskForm from './components/NewTaskForm';
 
-function App() {
-  return (
-    <>
-      <div className="App">
-        <header className="App-header">
-          <MenuBar />
-        </header>
-      </div>
-      <div className="App-body">
-        <Switch>
-          <Route exact path="/">
-            <Redirect to="/login" />
-          </Route>
-          <Route exact path="/login" component={NewSession} />
-          <Route exact path="/signup" component={NewAccount} />
-          <Route exact path="/tasks" component={Tasks} />
-        </Switch>
-      </div>
-    </>
-  );
-}
+const App = () => (
+  <>
+    <div className="App">
+      <header className="App-header">
+        <MenuBar />
+      </header>
+    </div>
+    <div className="App-body">
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/login" />
+        </Route>
+        <Route exact path="/login" component={NewSession} />
+        <Route exact path="/signup" component={NewAccount} />
+        <Route exact path="/tasks" component={Tasks} />
+        <Route exact path="/newtask" component={NewTaskForm} />
+      </Switch>
+    </div>
+    {useLocation().pathname !== '/newtask' ? <CreateBtn /> : null}
+  </>
+);
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
