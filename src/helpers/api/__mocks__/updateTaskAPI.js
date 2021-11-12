39327616320ecf04f18e3583fe37475e3f4f13cd
Binyam <binyam@pop-os.localdomain>
2021-11-12 09:41:15 +0300

    Add test for Tasks component and updateTaskAPI

--- src/helpers/api/__mocks__/updateTaskAPI.js
+++ src/helpers/api/__mocks__/updateTaskAPI.js
@@ -1,5 +1,12 @@
-const updateTaskAPI = (task) => Promise.resolve(
-
-);
-
+const updateTaskAPI = (taskName, description, progress, todos) => Promise.resolve(
+    {
+      json: () => ({
+        taskName,
+        description,
+        progress,
+        todos,
+      }),
+    },
+  );
+  
 export default updateTaskAPI;
