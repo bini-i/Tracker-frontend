506d75cc690f2a0b839d54da8df44aa38e4ea046
bini <bini@pop-os.localdomain>
2021-11-15 11:33:04 +0300

    Update linter styling indentations

--- src/helpers/api/__mocks__/updateTaskAPI.js
+++ src/helpers/api/__mocks__/updateTaskAPI.js
@@ -1,12 +1,12 @@
 const updateTaskAPI = (taskName, description, progress, todos) => Promise.resolve(
-    {
-      json: () => ({
-        taskName,
-        description,
-        progress,
-        todos,
-      }),
-    },
-  );
-  
+  {
+    json: () => ({
+      taskName,
+      description,
+      progress,
+      todos,
+    }),
+  },
+);
+
 export default updateTaskAPI;
